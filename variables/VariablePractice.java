ad4ce2c894a043e112ed1ac68504b6e451126e32
AiswaryalakshmiG <aiswaryalakahmi1502@gmail.com>
2025-09-14 21:44:39 +0530

    Learning Variables

---
+++ variables/VariablePractice.java
@@ -0,0 +1,16 @@
+public class VariablePractice {
+	int globalVariable = 10;
+
+	public static void main(String[] args) {
+		VariablePractice scope = new VariablePractice();
+		scope.display();
+		// System.out.println("Local variable: " + localVariable);
+	}
+
+	public void display() {
+		int localVariable = 20;
+		System.out.println("Global Variable: " + globalVariable);
+		System.out.println("local Variable: " + localVariable);
+	}
+
+}
